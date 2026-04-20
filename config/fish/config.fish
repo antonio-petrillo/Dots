412f88ef522d3ad834d8cfbe50e685be791ba0be
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-04-20 10:03:02 +0200

    add deps and alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set -g fish_user_paths "$HOME/.local/opt/Odin" $fish_user_paths
+alias sinp="setxkbmap -layout us,us -variant ,intl  -option ctrl:nocaps,grp:win_space_toggle && xinput set-prop 'PIXA3854:00 093A:0274 Touchpad' 'libinput Tapping Enabled' 1"
+
