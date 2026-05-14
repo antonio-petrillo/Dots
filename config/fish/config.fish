585b3fa38d19f4d0e1540fe5a5415d87765985e8
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2026-05-14 23:34:38 +0200

    sync f13

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set -g fish_user_paths "$HOME/.local/opt/Odin" $fish_user_paths
-alias sinp="setxkbmap -layout us,us -variant ,intl  -option ctrl:nocaps,grp:win_space_toggle && xinput set-prop 'PIXA3854:00 093A:0274 Touchpad' 'libinput Tapping Enabled' 1"
+alias sinp="setxkbmap -layout us,us -variant ,intl  -option ctrl:nocaps,grp:ctrls_toggle && xinput set-prop 'PIXA3854:00 093A:0274 Touchpad' 'libinput Tapping Enabled' 1"
 
